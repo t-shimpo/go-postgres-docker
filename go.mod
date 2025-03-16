e3f8974fded98250fc854c2e520ab839d989494e
t-shimpo <zanjibaru7@gmail.com>
2025-03-17 07:37:55 +0900

    postgreSQLとの接続を確認する

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/t-shimpo/go-postgres-docker
 
 go 1.24.0
+
+require github.com/lib/pq v1.10.9
--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL is not set")
+	}
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Database connection successful"))
 }
 
 func main() {
 	http.HandleFunc("/", handler)
 	fmt.Println("Server running on port 8080")
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
